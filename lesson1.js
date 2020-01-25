58d7b7954e07baf7f59fb0750bf3601f226aa26c
Sunny Patel <spp1991@gmail.com>
2020-01-25 15:20:12 -0500

    Removed unnecessary files

--- lesson1.js
+++
@@ -1,4 +0,0 @@
-var readlineSync = require("readline-sync");
-var degrees = Number(readlineSync.question("Enter Degrees in Celsius:"));
-var degreesFahrenheit = degrees  * 1.8 + 32;
-console.log(degreesFahrenheit)
